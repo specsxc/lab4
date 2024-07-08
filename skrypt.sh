38417c5101c0fac872ad8329932a848d47519e82
specsxc <piotr.dolgoszyja1@gmail.com>
2024-07-08 14:06:41 +0200

    add task4

--- skrypt.sh
+++ skrypt.sh
@@ -14,7 +14,7 @@ fi
 
 #task3
 
-if [[ "$1" == "--logs" ]]; then
+if [ "$1" == "--logs" ] && [ $# == 1 ]; then
     for i in {1..100}; do
         nazwa_pliku="log${i}.txt"
         echo "Nazwa pliku: $nazwa_pliku" > "$nazwa_pliku"
@@ -24,4 +24,19 @@ if [[ "$1" == "--logs" ]]; then
     echo "Utworzono 100 plików logx.txt"
 else
     echo "Użycie: $0 --logs"
+fi
+
+#task4
+
+if [ $# == 2 ] && [ "$1" == "--logs" ]; then
+    liczba_plikow=$2
+    for i in $(seq 1 "$liczba_plikow"); do
+        nazwa_pliku="log${i}.txt"
+        echo "Nazwa pliku: $nazwa_pliku" > "$nazwa_pliku"
+        echo "Nazwa skryptu: $0" >> "$nazwa_pliku"
+        echo "Data utworzenia: $(date)" >> "$nazwa_pliku"
+    done
+    echo "Utworzono $liczba_plikow plików logx.txt"
+else
+    echo "Użycie: skrypt.sh --logs <liczba_plików>"
 fi
