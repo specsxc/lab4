77897abab5f800456a46e0f9c8a19144175f755a
specsxc <piotr.dolgoszyja1@gmail.com>
2024-07-08 15:43:42 +0200

    Update --help flag

--- skrypt.sh
+++ skrypt.sh
@@ -30,9 +30,11 @@ case "$1" in
     ;;
     -h|--help)
         echo "Użycie: $0 [Opcje]"
-        echo "--date -d,       Wyświetla dzisiejszą date"
-        echo "--logs -l,   [liczba]    Tworzy podaną liczbę plików tekstowych 'log' (domyślnie 100)"
-        echo "--help -h,       Pokazuje instrukcje obsługi"
+        echo "--date, -d        Wyświetla dzisiejszą date"
+        echo "--logs, -l [liczba]       Tworzy podaną liczbę plików tekstowych 'log' (domyślnie 100)"
+        echo "--help, -h        Pokazuje instrukcje obsługi"
+        echo "--init        Klonuje repozytorium i ustawia zmienna PATH"
+        echo "--error, -e [liczba]      Tworzy podaną liczbę plików tekstowych 'error' (domyślnie: 100)"
     ;;
     *)
         echo "Użycie: $0 --help więcej informacji"
