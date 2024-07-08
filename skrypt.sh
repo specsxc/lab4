fcec1733a80e3073fb1ec9e8a4d2836a069d7da8
specsxc <piotr.dolgoszyja1@gmail.com>
2024-07-08 13:26:45 +0200

    task3

--- skrypt.sh
+++ skrypt.sh
@@ -2,9 +2,7 @@
 
 #task1
 
-data=$1
-
-if [[ "$data" == "--date" ]]; then
+if [[ "$1" == "--date" ]]; then
     date
 else
     echo "Użycie: $0 --date"
@@ -13,3 +11,17 @@ fi
 #task2
 
 #Add .gitignore to ignore files containing 'log'
+
+#task3
+
+if [[ "$1" == "--logs" ]]; then
+    for i in {1..100}; do
+        nazwa_pliku="log${i}.txt"
+        echo "Nazwa pliku: $nazwa_pliku" > "$nazwa_pliku"
+        echo "Nazwa skryptu: $0" >> "$nazwa_pliku"
+        echo "Data utworzenia: $(date)" >> "$nazwa_pliku"
+    done
+    echo "Utworzono 100 plików logx.txt"
+else
+    echo "Użycie: $0 --logs"
+fi
